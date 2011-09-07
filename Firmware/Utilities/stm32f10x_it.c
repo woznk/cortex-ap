25cf13d9465bd17fad2a919116525785f69028d9
rosenkranz@email.it <rosenkranz@email.it@71e6fc66-f7d7-ef58-60b5-af1553fe8d2f>
2011-09-07 15:04:26 +0000

    Removed (re)definition of SysTick_Handler()

--- Firmware/Utilities/stm32f10x_it.c
+++ Firmware/Utilities/stm32f10x_it.c
@@ -5,8 +5,8 @@
   * @version V3.1.2
   * @date    09/28/2009
   * @brief   Main Interrupt Service Routines.
-  *         This file provides template for all exceptions handler and
-  *         peripherals interrupt service routine.
+  *          This file provides template for all exceptions handler and
+  *          peripherals interrupt service routine.
   ******************************************************************************
   * @copy
   *
@@ -128,9 +128,10 @@ void PendSV_Handler(void)
   * @param  None
   * @retval None
   */
+/*
 void SysTick_Handler(void)
 {}
-
+*/
 /******************************************************************************/
 /*                 STM32F10x Peripherals Interrupt Handlers                   */
 /*  Add here the Interrupt Handler for the used peripheral(s) (PPP), for the  */
