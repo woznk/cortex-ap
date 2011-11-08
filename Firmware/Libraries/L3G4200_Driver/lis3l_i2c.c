e6a19ff092fb4f5dbdb9f6abb4fc9bc45dfbd979
rosenkranz@email.it <rosenkranz@email.it@71e6fc66-f7d7-ef58-60b5-af1553fe8d2f>
2011-11-08 17:27:27 +0000

    Occurrences of "lis3l" replaced with "l3g"

--- Firmware/Libraries/L3G4200_Driver/lis3l_i2c.c
+++ Firmware/Libraries/L3G4200_Driver/lis3l_i2c.c
@@ -1,10 +1,10 @@
 /**
 ******************************************************************************
-* @file lis3l_i2c.c
+* @file l3g_i2c.c
 * @author Peter Lanius
 * @version V0.10
 * @date 09/12/2009
-* @brief This file provides functions to operate the LIS3LV02DQ 3-axis
+* @brief This file provides functions to operate the L3GV02DQ 3-axis
          accelerometer in I2C bus mode.
 ******************************************************************************
 * @copy
@@ -12,7 +12,7 @@
 
 /* Includes ----------------------------------------------------------*/
 
-#include "lis3l_i2c.h"
+#include "l3g_i2c.h"
 #include "stm32f10x_i2c. h"
 
 /* Private typedef ----------------------------------------------------------*/
@@ -39,11 +39,11 @@ void GPIO_Configuration( void)
 {
     GPIO_InitTypeDef GPIO_InitStructure;
 
-    /* Configure I2C_LIS3L pins: SCL and SDA */
-    GPIO_InitStructure. GPIO_Pin = I2C_LIS3L_SCL | I2C_LIS3L_SDA;
+    /* Configure I2C_L3G pins: SCL and SDA */
+    GPIO_InitStructure. GPIO_Pin = I2C_L3G_SCL | I2C_L3G_SDA;
     GPIO_InitStructure. GPIO_Speed = GPIO_Speed_50MHz;
     GPIO_InitStructure. GPIO_Mode = GPIO_Mode_AF_OD;
-    GPIO_Init(I2C_LIS3L_GPIO, &GPIO_InitStructure );
+    GPIO_Init(I2C_L3G_GPIO, &GPIO_InitStructure );
 }
 
 /**
@@ -64,14 +64,14 @@ void I2C_Configuration( void)
     I2C_InitStructure. I2C_ClockSpeed = I2C_Speed;
 
     /* I2C Peripheral Enable */
-    I2C_Cmd(I2C_LIS3L, ENABLE);
+    I2C_Cmd(I2C_L3G, ENABLE);
 
     /* Apply I2C configuration after enabling it */
-    I2C_Init(I2C_LIS3L, &I2C_InitStructure) ;
+    I2C_Init(I2C_L3G, &I2C_InitStructure) ;
 }
 
 /**
-* @brief Initializes peripherals used by the I2C LIS3L driver.
+* @brief Initializes peripherals used by the I2C L3G driver.
 * @param None
 * @retval None
 */
@@ -81,18 +81,18 @@ void I2C_Configuration( void)
 /* Private macro ----------------------------------------------------------*/
 #define countof(a) (sizeof(a) / sizeof(*(a)) )
 
-void I2C_LIS3L_Init( )
+void I2C_L3G_Init( )
 {
-    uint8_t TxBuffer1[] = "\n\rLIS3LV02DQ Initialisation OK.\n\r";
-    uint8_t TxBuffer2[] = "\n\rLIS3LV02DQ Initialisation FAILED.\n\r" ;
+    uint8_t TxBuffer1[] = "\n\rL3G4200D Initialisation OK.\n\r";
+    uint8_t TxBuffer2[] = "\n\rL3G4200D Initialisation FAILED.\n\r" ;
     uint8_t TxCounter = 0;
-    uint8_t lis3l_addr = 0;
+    uint8_t l3g_addr = 0;
 
     /* I2C Periph clock enable */
-    RCC_APB1PeriphClock Cmd(I2C_LIS3L_CLK, ENABLE);
+    RCC_APB1PeriphClock Cmd(I2C_L3G_CLK, ENABLE);
 
     /* GPIO Periph clock enable */
-    RCC_APB2PeriphClock Cmd(I2C_LIS3L_GPIO_CLK, ENABLE);
+    RCC_APB2PeriphClock Cmd(I2C_L3G_GPIO_CLK, ENABLE);
 
     /* GPIO configuration */
     GPIO_Configuration( );
@@ -101,13 +101,13 @@ void I2C_LIS3L_Init( )
     I2C_Configuration( );
 
     /* Initialise the device as per data sheet */
-    I2C_LIS3L_Write(LIS3L_CTRL_REG1, LIS3L_INIT_CODE) ;
-    I2C_LIS3L_Write(LIS3L_CTRL_REG2, 0x00);
+    I2C_L3G_Write(L3G_CTRL_REG1, L3G_INIT_CODE) ;
+    I2C_L3G_Write(L3G_CTRL_REG2, 0x00);
 
     /* check everything is ok by reading whoami register */
-    lis3l_addr = I2C_LIS3L_ReadByte( LIS3L_WHO_AM_I);
+    l3g_addr = I2C_L3G_ReadByte( L3G_WHO_AM_I);
 
-    if (lis3l_addr == LIS3L_SLAVE_ADDR) { /* Match - Send the relevant message */
+    if (l3g_addr == L3G_SLAVE_ADDR) { /* Match - Send the relevant message */
         while (TxCounter+ + < TxBuffer1Size)
         {
             USART_SendData( USART1, TxBuffer1[TxCounter - 1]);
@@ -122,42 +122,29 @@ void I2C_LIS3L_Init( )
     }
 
     /* Set the data rate to the desired rate */
-    I2C_LIS3L_Write( LIS3L_CTRL_REG1, LIS3L_RUN_CODE) ;
+    I2C_L3G_Write(L3G_CTRL_REG1, L3G_RUN_CODE) ;
 }
 
 /**
-* @brief Write a byte to the specified register of the LIS3L sensor
-* @param WriteAddr : 8bit write address of the LIS3L register
+* @brief Write a byte to the specified register of the L3G sensor
+* @param WriteAddr : 8bit write address of the L3G register
 * @param DataByte : byte to write to the specified register
 * @retval none
 */
-void I2C_LIS3L_Write( uint8_t WriteAddr, uint8_t DataByte)
+void I2C_L3G_Write( uint8_t WriteAddr, uint8_t DataByte)
 {
-    /* Send START condition */
-    I2C_GenerateSTART( I2C_LIS3L, ENABLE);
+    I2C_GenerateSTART(I2C_L3G, ENABLE);                                /* Send START condition */
+    while (!I2C_CheckEvent(I2C_L3G, I2C_EVENT_MASTER_MODE_SELECT) );   /* Test on EV5 and clear it */
 
-    /* Test on EV5 and clear it */
-    while(!I2C_CheckEve nt(I2C_LIS3L, I2C_EVENT_MASTER_MODE_SELECT) );
+    I2C_Send7bitAddress(I2C_L3G, L3G_SLAVE_ADDR, I2C_Direction_Transmitter); /* Send L3G address for write */
+    while (!I2C_CheckEvent(I2C_L3G, I2C_EVENT_MASTER_TRANSMITTER_MODE_SELECTED) );    /* Test on EV6 and clear it */
 
-    /* Send LIS3L address for write */
-    I2C_Send7bitAddress (I2C_LIS3L, LIS3L_SLAVE_ADDR, I2C_Direction_Transmitter) ;
+    I2C_SendData(I2C_L3G, WriteAddr);                                  /* Send the L3G's internal register address to write to */
+    while (!I2C_CheckEvent(I2C_L3G, I2C_EVENT_MASTER_BYTE_TRANSMITTED ));    /* Test on EV8 and clear it */
 
-    /* Test on EV6 and clear it */
-    while(!I2C_CheckEve nt(I2C_LIS3L, I2C_EVENT_MASTER_TRANSMITTER_MODE_SELECTED) );
+    I2C_SendData(I2C_L3G, DataByte);                                   /* Send the byte to be written */
+    while (!I2C_CheckEvent(I2C_L3G, I2C_EVENT_MASTER_BYTE_TRANSMITTED ));    /* Test on EV8 and clear it */
 
-    /* Send the LIS3L's internal register address to write to */
-    I2C_SendData( I2C_LIS3L, WriteAddr);
-
-    /* Test on EV8 and clear it */
-    while(!I2C_CheckEve nt(I2C_LIS3L, I2C_EVENT_MASTER_BYTE_TRANSMITTED ));
-
-    /* Send the byte to be written */
-    I2C_SendData( I2C_LIS3L, DataByte);
-
-    /* Test on EV8 and clear it */
-    while(!I2C_CheckEve nt(I2C_LIS3L, I2C_EVENT_MASTER_BYTE_TRANSMITTED ));
-
-    /* Send STOP condition */
-    I2C_GenerateSTOP( I2C_LIS3L, ENABLE);
+    I2C_GenerateSTOP(I2C_L3G, ENABLE);                                 /* Send STOP condition */
 }
 
