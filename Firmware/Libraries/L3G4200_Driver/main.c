b98ddf7fa8d75ad9e350a4fda2ee984cd40a44ab
rosenkranz@email.it <rosenkranz@email.it@71e6fc66-f7d7-ef58-60b5-af1553fe8d2f>
2011-11-14 17:42:52 +0000

    SPI_Mems_Init() replaced with I2C_Init()

--- Firmware/Libraries/L3G4200_Driver/main.c
+++ Firmware/Libraries/L3G4200_Driver/main.c
@@ -1,28 +1,14 @@
 /******************** (C) COPYRIGHT 2011 STMicroelectronics ********************
-* File Name          : main.c
-* Author             : MSH Application Team
-* Author             : andrea labombarda
-* Revision           : $Revision: 1.5 $
-* Date               : $Date: 2010/08/04 12:19:08 $
-* Description        : EKSTM32 main file
-* HISTORY:
-* Date        | Modification                                | Author
-* 16/06/2011  | Initial Revision                            | Andrea Labombarda
-
-********************************************************************************
-* THE PRESENT FIRMWARE WHICH IS FOR GUIDANCE ONLY AIMS AT PROVIDING CUSTOMERS
-* WITH CODING INFORMATION REGARDING THEIR PRODUCTS IN ORDER FOR THEM TO SAVE TIME.
-* AS A RESULT, STMICROELECTRONICS SHALL NOT BE HELD LIABLE FOR ANY DIRECT,
-* INDIRECT OR CONSEQUENTIAL DAMAGES WITH RESPECT TO ANY CLAIMS ARISING FROM THE
-* CONTENT OF SUCH FIRMWARE AND/OR THE USE MADE BY CUSTOMERS OF THE CODING
-* INFORMATION CONTAINED HEREIN IN CONNECTION WITH THEIR PRODUCTS.
-*
-* THIS SOFTWARE IS SPECIFICALLY DESIGNED FOR EXCLUSIVE USE WITH ST PARTS.
+* $HeadURL $
+* $Author: $
+* $Revision: 1.5 $
+* Changes: SPI_Mems_Init() replaced with I2C_Init()
 *
 *******************************************************************************/
 
 
 /* Includes ------------------------------------------------------------------*/
+
 #include "stm32f10x.h"
 #include "usb_lib.h"
 #include "usb_desc.h"
@@ -40,6 +26,7 @@
 /* Private define ------------------------------------------------------------*/
 /* Private macro -------------------------------------------------------------*/
 /* Private variables ---------------------------------------------------------*/
+
 uint8_t response;
 uint8_t USBbuffer[64];
 
@@ -65,68 +52,64 @@ int main(void)
 
   EKSTM32_LEDOff(LED1);
   EKSTM32_LEDOff(LED2);
-  EKSTM32_LEDOff(LED3);    
+  EKSTM32_LEDOff(LED3);
 
   //wait until the USB is ready
-  while(bDeviceState != CONFIGURED);  
+  while(bDeviceState != CONFIGURED);
 
-  //SPI peripheral initialization
-  SPI_Mems_Init();
+  //I2C peripheral initialization
+  I2C_Init();
 
   //set the ODR and Bandwith
   SetODR(ODR_100Hz_BW_12_5);
-  //enable all axis  
-  SetAxis(X_ENABLE | Y_ENABLE | Z_ENABLE);  
+  //enable all axis
+  SetAxis(X_ENABLE | Y_ENABLE | Z_ENABLE);
   //set the fullscale
   SetFullScale(FULLSCALE_250);
   //set sensor mode
   SetMode(NORMAL);
-  
-  //interrupt configuration
+
   //interrupt pin mode configuration: PUSH PULL
-  SetIntPinMode(PUSH_PULL);  
+  SetIntPinMode(PUSH_PULL);
   //enable interrutp 1 on INT1 pin and set interrupt active high
-  SetInt1Pin(I1_ON_PIN_INT1_ENABLE | INT1_ACTIVE_HIGH);  
-  //X and Y high threshold interrutps 
-  SetIntConfiguration(INT1_OR | INT1_ZHIE_ENABLE | INT1_XHIE_ENABLE);  
+  SetInt1Pin(I1_ON_PIN_INT1_ENABLE | INT1_ACTIVE_HIGH);
+  //X and Y high threshold interrutps
+  SetIntConfiguration(INT1_OR | INT1_ZHIE_ENABLE | INT1_XHIE_ENABLE);
   //interrupt latch disable
   Int1LatchEnable(MEMS_DISABLE);
-  //set the threshold only on the Z axis  
+  //set the threshold only on the Z axis
   SetInt1Threshold(THS_Z, 500);
   //set the duration to 2 odr
-  SetInt1Duration(2);  
-  
-  //FIFO configuration
-  //set the fifo mode
+  SetInt1Duration(2);
+
+  //FIFO configuration, set the fifo mode
   FIFOModeEnable(FIFO_MODE);
 
   //set watermark to 5
   SetWaterMark(5);
 
-  //enable watermark interrupt on interrupt2 
+  //enable watermark interrupt on interrupt2
   //when the fifo contains more than 5 elements, the interrupt raises
   SetInt2Pin(WTM_ON_INT2_ENABLE);
 
-  while(1) {
-    //Polling data reading
+  while (1) {
     //check if there is some data available
     GetSatusReg(&status);
-    if( ValBit(status, DATAREADY_BIT) ){
-      
+    if (ValBit(status, DATAREADY_BIT)) {
+
       //get x, y, z angular rate raw data
-      //the sensitivity is not applied yet
       GetAngRateRaw(&buff);
 
       //Led blinking
-      if( (counter >= 0) && (counter < 10)) {
+      if ((counter >= 0) && (counter < 10)) {
         EKSTM32_LEDToggle(LED3);
       }
-      counter = (counter+1) % 100;
+      counter = (counter + 1) % 100;
 
       //print on USB
       memset(USBbuffer, '\0', 64);
       sprintf((char*)USBbuffer, "X=%6d Y=%6d Z=%6d \r\n", buff.x, buff.y, buff.z);
- 
+
       USB_SIL_Write(EP1_IN, USBbuffer, 29 );
       SetEPTxValid(ENDP1);
     }
