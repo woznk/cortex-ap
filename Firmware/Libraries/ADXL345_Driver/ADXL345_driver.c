97fc4ae97aee7676c56128d208d42603206eeb4e
rosenkranz@email.it <rosenkranz@email.it@71e6fc66-f7d7-ef58-60b5-af1553fe8d2f>
2011-11-22 22:30:58 +0000

    Added function GetAccelRaw(), filled function ADXL345_Init()

--- Firmware/Libraries/ADXL345_Driver/ADXL345_driver.c
+++ Firmware/Libraries/ADXL345_Driver/ADXL345_driver.c
@@ -1,77 +1,51 @@
-/*******************************************************************************
-* File Name:
-* $Revision:$
-* $Date:$
-* driver file
-* Change:
-*
-********************************************************************************/
+//============================================================================+
+//
+// $RCSfile: $
+// $Revision: $
+// $Date: $
+// $Author: $
+/// \brief I2C driver for MEMS sensors
+//  Change: Added function GetAccelRaw(), filled function ADXL345_Init()
+//
+//============================================================================*/
 
 #include "i2c_mems_driver.h"
 #include "adxl345_driver.h"
 
-/* Private typedef -----------------------------------------------------------*/
-/* Private define ------------------------------------------------------------*/
-/* Private macro -------------------------------------------------------------*/
-/* Private variables ---------------------------------------------------------*/
-/* Private function prototypes -----------------------------------------------*/
-/* Private functions ---------------------------------------------------------*/
+/*--------------------------------- Definitions ------------------------------*/
 
+#ifdef VAR_STATIC
+#undef VAR_STATIC
+#endif
+#define VAR_STATIC static
+#ifdef VAR_GLOBAL
+#undef VAR_GLOBAL
+#endif
+#define VAR_GLOBAL
 
-void ADXL345_Init( void )
-{
+/*----------------------------------- Macros ---------------------------------*/
 
-  // Configure ADXL345 registers
+/*-------------------------------- Enumerations ------------------------------*/
 
-}
+/*----------------------------------- Types ----------------------------------*/
 
-status_t Get_Output_Rate( uint16_t *rate_in_hz )
-{
-  uint8_t value;
+/*---------------------------------- Constants -------------------------------*/
 
-  if (!ReadReg(ADXL345_SLAVE_ADDR, BW_RATE, &value))
-    return MEMS_ERROR;
+/*---------------------------------- Globals ---------------------------------*/
 
-  value &= 0x0F;
-  switch (value) {
-    case 0x06:
-        rate_in_hz = 6;
-    break;
-    case 0x07:
-        rate_in_hz = 12;
-    break;
-    case 0x08:
-        rate_in_hz = 25;
-    break;
-    case 0x09:
-        rate_in_hz = 50;
-    break;
-    case 0x0A:
-        rate_in_hz = 100;
-    break;
-    case 0x0B:
-        rate_in_hz = 200;
-    break;
-    case 0x0C:
-        rate_in_hz = 400;
-    break;
-    case 0x0D:
-        rate_in_hz = 800;
-    break;
-    case 0x0E:
-        rate_in_hz = 1600;
-    break;
-    case 0x0F:
-        rate_in_hz = 3200;
-    break;
-    default:
-        return MEMS_ERROR;
-    break;
-  }
-  return MEMS_SUCCESS;
-}
+/*----------------------------------- Locals ---------------------------------*/
 
-status_t Set_Output_Rate( uint8_t rate_code )
+/*--------------------------------- Prototypes -------------------------------*/
+
+///----------------------------------------------------------------------------
+///
+/// \brief   Set ADXL345 output rate and bandwidth
+/// \return  MEMS_SUCCESS / MEMS_ERROR
+/// \param   rate_code, output rate code
+/// \remarks -
+///
+///----------------------------------------------------------------------------
+static status_t Set_Output_Rate( uint8_t rate_code )
 {
   uint8_t value;
 
@@ -87,7 +61,15 @@ status_t Set_Output_Rate( uint8_t rate_code )
   return MEMS_SUCCESS;
 }
 
-status_t Start_Measurement( uint8_t rate )
+///----------------------------------------------------------------------------
+///
+/// \brief   Start ADXL345 measurement
+/// \return  MEMS_SUCCESS / MEMS_ERROR
+/// \param   -
+/// \remarks -
+///
+///----------------------------------------------------------------------------
+static status_t Start_Measurement( void )
 {
   uint8_t value;
 
@@ -98,6 +80,58 @@ status_t Start_Measurement( uint8_t rate )
 
   if (!WriteReg(ADXL345_SLAVE_ADDR, POWER_CTL, value))
     return MEMS_ERROR;
+
+  return MEMS_SUCCESS;
+}
+
+///----------------------------------------------------------------------------
+///
+/// \brief   Initialization of ADXL345 accelerometer
+/// \return  -
+/// \remarks -
+///
+///----------------------------------------------------------------------------
+void ADXL345_Init( void )
+{
+  Set_Output_Rate(0x09);    // Set Output Rate to 100 Hz
+  Start_Measurement( );     // Start measurement
+}
+
+///----------------------------------------------------------------------------
+///
+/// \brief   Read the acceleration registers
+/// \return  MEMS_SUCCESS / MEMS_ERROR
+/// \param   *buff, pointer to acceleration structure
+/// \remarks -
+///
+///----------------------------------------------------------------------------
+status_t GetAccelRaw(AccelRaw_t* buff) {
+  unsigned char valueL;
+  unsigned char valueH;
+
+  if (!ReadReg(ADXL345_SLAVE_ADDR, DATAX0, &valueL) )
+      return MEMS_ERROR;
+
+  if (!ReadReg(ADXL345_SLAVE_ADDR, DATAX1, &valueH) )
+      return MEMS_ERROR;
+
+  buff->x = (short int)((valueH << 8) | valueL );
+
+  if (!ReadReg(ADXL345_SLAVE_ADDR, DATAY0, &valueL) )
+      return MEMS_ERROR;
+
+  if (!ReadReg(ADXL345_SLAVE_ADDR, DATAY1, &valueH) )
+      return MEMS_ERROR;
+
+  buff->y = (short int)((valueH << 8) | valueL );
+
+   if (!ReadReg(ADXL345_SLAVE_ADDR, DATAZ0, &valueL) )
+      return MEMS_ERROR;
+
+  if (!ReadReg(ADXL345_SLAVE_ADDR, DATAZ1, &valueH) )
+      return MEMS_ERROR;
+
+  buff->z = (short int)((valueH << 8) | valueL );
 
   return MEMS_SUCCESS;
 }
