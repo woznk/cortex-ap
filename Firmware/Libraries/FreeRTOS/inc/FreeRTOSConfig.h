c1d1f80c0ef344783a7e623f50eeee3065242b17
rosenkranz@email.it <rosenkranz@email.it@71e6fc66-f7d7-ef58-60b5-af1553fe8d2f>
2011-12-20 16:00:49 +0000

    reduced configMINIMAL_STACK_SIZE from 128 to 16

--- Firmware/Libraries/FreeRTOS/inc/FreeRTOSConfig.h
+++ Firmware/Libraries/FreeRTOS/inc/FreeRTOSConfig.h
@@ -75,7 +75,7 @@
 #define configCPU_CLOCK_HZ              ( ( unsigned long ) 8000000 )   // original: 72000000
 #define configTICK_RATE_HZ              ( ( portTickType ) 1000 )
 #define configMAX_PRIORITIES            ( ( unsigned portBASE_TYPE ) 5 )
-#define configMINIMAL_STACK_SIZE        ( ( unsigned short ) 128 )
+#define configMINIMAL_STACK_SIZE        ( ( unsigned short ) 16 )       // original: 128
 #define configTOTAL_HEAP_SIZE           ( ( size_t ) ( 1024 ) )         // original: 17 * 1024
 #define configMAX_TASK_NAME_LEN         ( 16 )                          // original: 32
 #define configUSE_TRACE_FACILITY        0
