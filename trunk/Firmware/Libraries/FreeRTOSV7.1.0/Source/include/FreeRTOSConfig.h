8b8fb074cfdd6617c9e4107a09b005ac48acfd1c
rosenkranz@email.it <rosenkranz@email.it@71e6fc66-f7d7-ef58-60b5-af1553fe8d2f>
2011-12-24 18:29:15 +0000

    increased tick rate to 1000 Hz

--- trunk/Firmware/Libraries/FreeRTOSV7.1.0/Source/include/FreeRTOSConfig.h
+++ trunk/Firmware/Libraries/FreeRTOSV7.1.0/Source/include/FreeRTOSConfig.h
@@ -73,7 +73,7 @@
 #define configUSE_IDLE_HOOK             0
 #define configUSE_TICK_HOOK             0
 #define configCPU_CLOCK_HZ              ( ( unsigned long ) 24000000 )   // original: 72000000
-#define configTICK_RATE_HZ              ( ( portTickType ) 100 )
+#define configTICK_RATE_HZ              ( ( portTickType ) 1000 )
 #define configMAX_PRIORITIES            ( ( unsigned portBASE_TYPE ) 5 )
 #define configMINIMAL_STACK_SIZE        ( ( unsigned short ) 128 )      // original: 128
 #define configTOTAL_HEAP_SIZE           ( ( size_t ) ( 2 * 1024 ) )         // original: 17 * 1024
