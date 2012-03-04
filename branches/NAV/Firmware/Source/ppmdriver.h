abf2f6e67795d6d41ed0db9c200bc62d64228975
rosenkranz@email.it <rosenkranz@email.it@71e6fc66-f7d7-ef58-60b5-af1553fe8d2f>
2012-03-04 22:21:05 +0000

    modified channel definitions for EasyStar

--- branches/NAV/Firmware/Source/ppmdriver.h
+++ branches/NAV/Firmware/Source/ppmdriver.h
@@ -8,7 +8,7 @@
 /// \brief
 /// PPM input driver header file
 ///
-//  CHANGES added channel definitions for different aircraft models
+//  CHANGES modified channel definitions for EasyStar
 //
 //============================================================================*/
 
@@ -29,7 +29,7 @@
 #define EASYSTAR            1
 #define TYCHO               2
 #define LEUKO               3
-#define MODEL               LEUKO
+#define MODEL               EASYSTAR
 
 #if (MODEL == TYCHO)
 #define AILERON_CHANNEL     0   // Aileron control channel
@@ -50,10 +50,14 @@
 #define KI_CHANNEL          6   // PID Ki channel
 #define RC_CHANNELS         7   // Number of channels, modify according to RC
 #elif (MODEL == EASYSTAR)
-#define RUDDER_CHANNEL      1   // Rudder control channel
+#define THROTTLE_CHANNEL    0   // Throttle control channel
 #define ELEVATOR_CHANNEL    2   // Elevator control channel
+#define RUDDER_CHANNEL      3   // Rudder control channel
 #define MODE_CHANNEL        4   // Mode selection channel
-#define RC_CHANNELS         6   // Number of channels, modify according to RC
+#define AILERON_CHANNEL     5   // Aileron channel
+#define KP_CHANNEL          6   // PID Kp channel
+#define KI_CHANNEL          8   // PID Ki channel
+#define RC_CHANNELS         7   // Number of channels, modify according to RC
 #else
 #error Aircraft model undefined !
 #endif
