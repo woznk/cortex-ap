7e4d63d51ffcbe81e60353cb82015773f223fc4f
rosenkranz@email.it <rosenkranz@email.it@71e6fc66-f7d7-ef58-60b5-af1553fe8d2f>
2012-04-01 21:17:17 +0000

    renamed interface functions

--- branches/NAV/Firmware/Source/nav.h
+++ branches/NAV/Firmware/Source/nav.h
@@ -7,7 +7,7 @@
 //
 /// \file
 /// \brief  Navigation task header file
-//  CHANGES added function Nav_Altitude()
+//  CHANGES renamed interface functions
 //
 //============================================================================
 
@@ -32,11 +32,11 @@
 
 void Navigation_Task( void *pvParameters );
 void Navigation_Init( void );
-int16_t Nav_Bearing ( void );
-float Nav_Altitude ( void );
+float Nav_Bearing ( void );
+float Nav_Bank ( void );
 uint16_t Nav_Heading ( void );
 uint16_t Nav_Distance ( void );
-uint16_t Nav_WaypointIndex ( void );
+uint16_t Nav_Wpt_Index ( void );
+uint16_t Nav_Wpt_Altitude ( void );
 uint16_t Nav_Ground_Speed ( void );
-void Nav_Gps_Putc( char c );
 
