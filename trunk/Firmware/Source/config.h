e74fa80618ecc80740dc4dd68596de47b7534cc8
rosenkranz@email.it <rosenkranz@email.it@71e6fc66-f7d7-ef58-60b5-af1553fe8d2f>
2012-07-25 21:19:31 +0000

    modified altitude hold parameters, rad <-> deg conversion macros moved here from DCM.c

--- trunk/Firmware/Source/config.h
+++ trunk/Firmware/Source/config.h
@@ -9,11 +9,15 @@
 ///
 /// \file
 ///
-// CHANGES reduced ALT_HOLD_THROTTLE_MAX to 0.3
+// CHANGES modified altitude hold parameters, 
+//         rad <-> deg conversion macros moved here from DCM.c
 //
 //============================================================================*/
 
-#define PI          3.141592f
+#define PI        3.141592f
+
+#define ToRad(x) (((x) * PI) / 180.0f)
+#define ToDeg(x) (((x) * 180.0f) / PI)
 
 /* Frequency of attitude control loop */
 #ifdef _WINDOWS
@@ -73,27 +77,27 @@
 /* The range of altitude within which to linearly vary the throttle
    and pitch to maintain altitude. Bigger values makes altitude hold
    smoother, and is suggested for very fast planes. */
-#define HEIGHT_MARGIN       20.0f
+#define HEIGHT_MARGIN   20.0f
 
 /* Use ALT_HOLD_THROTTLE_MAX when below HEIGHT_MARGIN of the target height.
    Interpolate between ALT_HOLD_THROTTLE_MAX and ALT_HOLD_THROTTLE_MIN
    when within HEIGHT_MARGIN of the target height.
    Use ALT_HOLD_THROTTLE_MIN when above HEIGHT_MARGIN of the target height.
    Throttle values are from -1.0 to +1.0.*/
-#define ALT_HOLD_THROTTLE_MIN   -0.3f
-#define ALT_HOLD_THROTTLE_MAX   0.3f
+#define ALT_HOLD_THROTTLE_MIN   -0.2f
+#define ALT_HOLD_THROTTLE_MAX   0.4f
 
-#define HEIGHTMAX           300.0f      //! maximum target height [m]
+#define HEIGHTMAX           1500.0f     //! maximum target height [m]
 
 #define HEIGHT_MARGIN       20.0f       //! height margin [m]
 
 #define MINIMUMTHROTTLE     -0.3f       //! minimum throttle
 
-#define PITCHATMINTHROTTLE  -0.0349f    //! Pitch angle at minimum throttle [rad]
+#define PITCHATMINTHROTTLE  ToRad(0.1f) //! Pitch angle at minimum throttle [rad]
 
-#define PITCHATMAXTHROTTLE  0.2094f     //! Pitch angle at maximum throttle [rad]
+#define PITCHATMAXTHROTTLE  ToRad(8.0f) //! Pitch angle at maximum throttle [rad]
 
-#define PITCHATZEROTHROTTLE -0.0872f    //! Pitch angle while gliding [rad]
+#define PITCHATZEROTHROTTLE ToRad(-6.0f)//! Pitch angle while gliding [rad]
 
 #define SIM_NONE    0                   // No simulator
 #define XPLANE      1                   // Simulator X-Plane
