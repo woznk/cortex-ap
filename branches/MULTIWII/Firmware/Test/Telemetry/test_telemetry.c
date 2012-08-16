02eb680e2a2a9fbb93b4f08412d6f88fc12fb604
rosenkranz@email.it <rosenkranz@email.it@71e6fc66-f7d7-ef58-60b5-af1553fe8d2f>
2012-08-16 08:56:49 +0000

    removed #inclusion of freertos files, main program cycles through test steps by calling USART1_Init function

--- branches/MULTIWII/Firmware/Test/Telemetry/test_telemetry.c
+++ branches/MULTIWII/Firmware/Test/Telemetry/test_telemetry.c
@@ -7,21 +7,18 @@
 //
 /// \brief test program
 ///
-// Change:
+// Change: removed #inclusion of freertos files, main program cycles through
+//         test steps by calling USART1_Init function
 //
 //============================================================================*/
 
 #include <stdint.h>
 
-#include "FreeRTOS.h"
-#include "task.h"
-#include "queue.h"
-#include "semphr.h"
-
-//#include "stm32f10x.h"
+#include "stm32f10x.h"
 
 #include "config.h"
-#include "telemetry.h"
+#include "usart1driver.h"
+#include "multiwii.h"
 
 /** @addtogroup test
   * @{
@@ -42,9 +39,6 @@
 #endif
 #define VAR_GLOBAL
 
-/* Task priorities. */
-#define mainTELEMETRY_PRIORITY  ( tskIDLE_PRIORITY + 2 )
-
 /*----------------------------------- Macros ---------------------------------*/
 
 /*-------------------------------- Enumerations ------------------------------*/
@@ -59,23 +53,8 @@
 
 /*--------------------------------- Prototypes -------------------------------*/
 
-void RCC_Configuration(void);
-void GPIO_Configuration(void);
-
 /*--------------------------------- Functions --------------------------------*/
 
-///----------------------------------------------------------------------------
-///
-/// \brief   hook for stack overflow check
-/// \return  -
-/// \remarks -
-///
-///----------------------------------------------------------------------------
-void vApplicationStackOverflowHook( xTaskHandle *pxTask, int8_t *pcTaskName ) {
-    ( void ) pxTask;
-    ( void ) pcTaskName;
-    while (1);
-}
 
 ///----------------------------------------------------------------------------
 ///
@@ -86,111 +65,31 @@ void vApplicationStackOverflowHook( xTaskHandle *pxTask, int8_t *pcTaskName ) {
 ///----------------------------------------------------------------------------
 int32_t main(void)
 {
-/*
-  NVIC_PriorityGroupConfig(NVIC_PriorityGroup_4);   // Configure priority group
-  RCC_Configuration();                              // System Clocks Configuration
-  GPIO_Configuration();                             // GPIO Configuration
-  xTaskCreate(Telemetry_Task, ( signed portCHAR * ) "Telemetry", 64, NULL, mainTELEMETRY_PRIORITY, NULL);
-  vTaskStartScheduler();
-*/
-
-  while (1) {
-     Telemetry_Task(0);
+  USART1_Init();
+  while (Test_Running()) {
+     MSP_Receive();
+  }
+  USART1_Init();
+  while (Test_Running()) {
+     MSP_Receive();
+  }
+  USART1_Init();
+  while (Test_Running()) {
+     MSP_Receive();
+  }
+  USART1_Init();
+  while (Test_Running()) {
+     MSP_Receive();
+  }
+  USART1_Init();
+  while (Test_Running()) {
+     MSP_Receive();
+  }
+  USART1_Init();
+  while (Test_Running()) {
+     MSP_Receive();
   }
 }
-
-///----------------------------------------------------------------------------
-///
-/// \brief   Configure the different system clocks.
-/// \return  -
-/// \remarks -
-///
-///----------------------------------------------------------------------------
-void RCC_Configuration(void)
-{
-/*
-  RCC_HCLKConfig(RCC_SYSCLK_Div1);              // HCLK = SYSCLK
-  RCC_PCLK1Config(RCC_HCLK_Div2);               // PCLK1 = HCLK/2
-  RCC_PCLK2Config(RCC_HCLK_Div1);               // PCLK2 = HCLK
-  RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM2 |  // TIM 2 used for PPM signal capture
-                         RCC_APB1Periph_TIM3 |  // TIM 3 used for servo signal PWM
-                         RCC_APB1Periph_USART2, // USART 2 used for GPS communication
-                         ENABLE);
-  RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA | // TIM2, TIM3, USART1, USART2
-                         RCC_APB2Periph_GPIOB | // TIM3
-                         RCC_APB2Periph_GPIOC | // LED
-                         RCC_APB2Periph_AFIO  | //
-                         RCC_APB2Periph_USART1, // USART 1 used for telemetry
-                         ENABLE);
-*/
-}
-
-///----------------------------------------------------------------------------
-///
-/// \brief   Configure pins.
-/// \return  -
-/// \remarks -
-///
-///----------------------------------------------------------------------------
-void GPIO_Configuration(void)
-{
-/*
-  GPIO_InitTypeDef GPIO_InitStructure;
-
-  // --------------- GPIOA Configuration -----------------
-
-  // TIM2 Channel 2 as input pull down (1)
-  GPIO_InitStructure.GPIO_Pin = GPIO_Pin_1;
-  GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IPD;
-  GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
-  GPIO_Init(GPIOA, &GPIO_InitStructure);
-
-  // TIM3 Channel 1, 2 as alternate function push-pull (6, 7)
-  GPIO_InitStructure.GPIO_Pin = GPIO_Pin_6 | GPIO_Pin_7;
-  GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF_PP;
-  GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
-  GPIO_Init(GPIOA, &GPIO_InitStructure);
-
-  // USART 1 TX pin as alternate function push pull (9)
-  GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF_PP;
-  GPIO_InitStructure.GPIO_Pin = GPIO_Pin_9;
-  GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
-  GPIO_Init(GPIOA, &GPIO_InitStructure);
-
-  // USART 1 RX pin as input floating (10)
-  GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IN_FLOATING;
-  GPIO_InitStructure.GPIO_Pin = GPIO_Pin_10;
-  GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
-  GPIO_Init(GPIOA, &GPIO_InitStructure);
-
-  // USART 2 TX pin as alternate function push pull (2)
-  GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF_PP;
-  GPIO_InitStructure.GPIO_Pin = GPIO_Pin_2;
-  GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
-  GPIO_Init(GPIOA, &GPIO_InitStructure);
-
-  // USART 2 RX pin as input floating (3)
-  GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IN_FLOATING;
-  GPIO_InitStructure.GPIO_Pin = GPIO_Pin_3;
-  GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
-  GPIO_Init(GPIOA, &GPIO_InitStructure);
-
-  // --------------- GPIOB Configuration -----------------
-
-  // TIM3 Channel 3, 4 as alternate function push-pull
-  GPIO_InitStructure.GPIO_Pin = GPIO_Pin_0 | GPIO_Pin_1;
-  GPIO_Init(GPIOB, &GPIO_InitStructure);
-
-  // --------------- GPIOC Configuration -----------------
-
-  // LED pins as push pull outputs
-  GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;
-  GPIO_InitStructure.GPIO_Pin = GPIO_Pin_8 | GPIO_Pin_9;
-  GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
-  GPIO_Init(GPIOC, &GPIO_InitStructure);
-*/
-}
-
 
 #ifdef  USE_FULL_ASSERT
 ///----------------------------------------------------------------------------
