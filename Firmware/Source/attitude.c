a0ed9c788c7fc08a80d0d495a150a34528b2d59f
rosenkranz@email.it <rosenkranz@email.it@71e6fc66-f7d7-ef58-60b5-af1553fe8d2f>
2012-09-16 19:09:00 +0000

    merged from MULTIWII branch

--- Firmware/Source/attitude.c
+++ Firmware/Source/attitude.c
@@ -7,7 +7,9 @@
 //
 /// \brief attitude control
 ///
-// Change: removed minor defects detectd by static analysis
+/// \file
+///
+// Change: added option for multiwii telemetry or ardupilot-like telemetry
 //
 //============================================================================*/
 
@@ -27,14 +29,16 @@
 
 #include "config.h"
 #include "dcm.h"
+/* uncomment telemetry type that applies */
 #include "telemetry.h"
+//#include "multiwii.h"
 #include "log.h"
 #include "led.h"
 #include "nav.h"
 #include "pid.h"
 #include "attitude.h"
 
-/** @addtogroup cortex-ap
+/** @addtogroup cortex_ap
   * @{
   */
 
@@ -53,11 +57,6 @@
 #endif
 #define VAR_GLOBAL
 
-#define LOG_SENSORS       0
-#define LOG_DCM           0
-#define LOG_PPM           0
-#define LOG_SERVO         0
-
 /*----------------------------------- Macros ---------------------------------*/
 
 /*-------------------------------- Enumerations ------------------------------*/
@@ -66,7 +65,8 @@
 
 /*---------------------------------- Constants -------------------------------*/
 
-VAR_STATIC const int16_t Sensor_Sign[6] = {
+/// sign of sensor data
+VAR_STATIC const int16_t iSensor_Sign[6] = {
     -1,     // acceleration X, must be positive forward
      1,     // acceleration Y, must be positive rightward
      1,     // acceleration Z, must be positive downward
@@ -79,25 +79,23 @@ VAR_STATIC const int16_t Sensor_Sign[6] = {
 
 /*----------------------------------- Locals ---------------------------------*/
 
-VAR_STATIC bool bTuning = FALSE;
-VAR_STATIC int16_t iAileron;
-VAR_STATIC int16_t iElevator;
-VAR_STATIC int16_t iThrottle;
-VAR_STATIC uint8_t ucBlink_Red = 0;
-VAR_STATIC uint8_t ucBlink_Blue = 0;
-VAR_STATIC uint8_t ucSensor_Data[16];
-//VAR_STATIC int16_t iMessage_Buffer[9];
-VAR_STATIC int16_t iSensor_Offset[6] = {0, 0, 0, 0, 0, 0};
-VAR_STATIC xPID Roll_Pid;
-VAR_STATIC xPID Pitch_Pid;
-//VAR_STATIC xLog_Message message;
-VAR_STATIC float fSetpoint;
-VAR_STATIC float fInput;
-VAR_STATIC float fOutput;
+VAR_STATIC bool bTuning = FALSE;        //!< in flight PID tuning activated
+VAR_STATIC int16_t iAileron;            //!< aileron servo position
+VAR_STATIC int16_t iElevator;           //!< elevator servo position
+VAR_STATIC int16_t iThrottle;           //!< throttle servo position
+VAR_STATIC uint8_t ucBlink_Red = 0;     //!< red LED blinking counter
+VAR_STATIC uint8_t ucBlink_Blue = 0;    //!< blue LED blinking counter
+VAR_STATIC xPID Roll_Pid;               //!< roll PID
+VAR_STATIC xPID Pitch_Pid;              //!< pitch PID
+VAR_STATIC float fSetpoint;             //!< PID setpoint
+VAR_STATIC float fInput;                //!< PID input
+VAR_STATIC float fOutput;               //!< PID output
+VAR_STATIC uint8_t ucSensor_Data[12];   //!< raw IMU data
+VAR_STATIC int16_t iSensor_Offset[6] =  //!< IMU sensors offset
+{0, 0, 0, 0, 0, 0};
 
 /*--------------------------------- Prototypes -------------------------------*/
 
-void AHRS_Task(void *pvParameters);
 static __inline void Attitude_Control(void);
 
 /*--------------------------------- Functions --------------------------------*/
@@ -150,9 +148,9 @@ void Attitude_Task(void *pvParameters)
         vTaskDelayUntil(&Last_Wake_Time, configTICK_RATE_HZ / SAMPLES_PER_SECOND);
 #if (SIMULATOR == SIM_NONE)                         // normal mode
         GetAccelRaw(ucSensor_Data);                 // acceleration
-        GetAngRateRaw((uint8_t *)&ucSensor_Data[6]);// rotation
+        GetAngRateRaw((uint8_t *)&ucSensor_Data[6]); // rotation
 #else                                               // simulation mode
-        Telemetry_Get_Sensors((int16_t *)ucSensor_Data);// get simulator sensors
+        Telemetry_Get_Raw_IMU((int16_t *)ucSensor_Data);// get simulator sensors
 #endif
         pSensor = (int16_t *)ucSensor_Data;
         for (j = 0; j < 6; j++) {                   // accumulate
@@ -179,13 +177,13 @@ void Attitude_Task(void *pvParameters)
         GetAngRateRaw((uint8_t *)&ucSensor_Data[6]);// rotation
         BMP085_Handler();
 #else                                               // simulation mode
-        Telemetry_Get_Sensors((int16_t *)ucSensor_Data);// get simulator sensors
+        Telemetry_Get_Raw_IMU((int16_t *)ucSensor_Data);// get simulator sensors
 #endif
         /* Offset and sign correction */
         pSensor = (int16_t *)ucSensor_Data;
         for (j = 0; j < 6; j++) {
             *pSensor = *pSensor - iSensor_Offset[j];// strip offset
-            *pSensor = *pSensor * Sensor_Sign[j];   // correct sign
+            *pSensor = *pSensor * iSensor_Sign[j];  // correct sign
             if (j == 2) {                           // z acceleration
                *pSensor += (int16_t)GRAVITY;        // add gravity
             }
