ec8f176ceb5a4292b578c71dc299b2c8b6d6f7ef
rosenkranz@email.it <rosenkranz@email.it@71e6fc66-f7d7-ef58-60b5-af1553fe8d2f>
2012-09-16 19:20:56 +0000

    merged from MULTIWII branch

--- Firmware/Source/log.h
+++ Firmware/Source/log.h
@@ -6,7 +6,10 @@
 // $Author: $
 //
 /// \brief   Log manager header file
-//  CHANGES  Log task moved to Log.c
+///
+/// \file
+///
+//  Change   Log task moved to Log.c
 //
 //============================================================================
 
@@ -23,23 +26,20 @@
 
 /*------------------------------------ Types ---------------------------------*/
 
+/// log message structure
 typedef struct
 {
-  uint8_t ucLength;
-  uint16_t *pcData;
+  uint8_t ucLength;   //!< length of message
+  uint16_t *pcData;   //!< pointer to message content
 } xLog_Message;
 
 /*---------------------------------- Constants -------------------------------*/
 
 /*----------------------------------- Globals --------------------------------*/
 
+/// queue of log messages
 VAR_GLOBAL xQueueHandle xLog_Queue;
 
 /*---------------------------------- Interface -------------------------------*/
 
-void Log_Init ( void );
-void Log_DCM ( void );
-void Log_PPM ( void );
-void Log_PutChar( char c );
-void Log_Send(uint16_t *data, uint8_t num);
 void Log_Task( void *pvParameters );
