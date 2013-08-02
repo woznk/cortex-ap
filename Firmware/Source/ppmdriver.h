cbdcfcf00d6230c8a915d39183c9517637c4d108
rosenkranz@email.it <rosenkranz@email.it@71e6fc66-f7d7-ef58-60b5-af1553fe8d2f>
2013-08-02 16:32:08 +0000

    #definition of aircraft model moved to config.h added channel #definitions for EPP FPV

--- Firmware/Source/ppmdriver.h
+++ Firmware/Source/ppmdriver.h
@@ -9,9 +9,8 @@
 ///
 /// \file
 ///
-//  Change  result of merge of NAV branch:
-//          modes MODE_ROLL_TUNE and MODE_PITCH_TUNE renamed MODE_STAB and
-//          MODE_NAV respectively
+//  Change  #definition of aircraft model moved to config.h
+//          added channel #definitions for EPP FPV
 //
 //============================================================================*/
 
@@ -28,12 +27,6 @@
 
 #define PPM_SIGNAL_OK       6   ///< radio signal is OK
 #define PPM_NO_SIGNAL       0   ///< radio signal is bad
-
-/* Aircraft model type definitions */
-#define EASYSTAR            1   ///< Easystar, 3 axis, aileron direction control
-#define TYCHO               2   ///< Tycho, 2 axis, rudder only direction control
-#define LEUKO               3   ///< Leuko, flying wing, rudder and aileron mixed
-#define MODEL               EASYSTAR    ///< current model
 
 /* RC channel definitions */
 #if (MODEL == TYCHO)
@@ -63,6 +56,15 @@
 #define KP_CHANNEL          6   ///< PID Kp channel
 #define KI_CHANNEL          8   ///< PID Ki channel
 #define RC_CHANNELS         7   ///< Number of channels, modify according to RC
+#elif (MODEL == EPPFPV)
+#define AILERON_CHANNEL     0   ///< Aileron channel
+#define ELEVATOR_CHANNEL    1   ///< Elevator control channel
+#define THROTTLE_CHANNEL    2   ///< Throttle control channel
+#define RUDDER_CHANNEL      3   ///< Rudder control channel
+#define MODE_CHANNEL        4   ///< Mode selection channel
+#define KP_CHANNEL          6   ///< PID Kp channel
+#define KI_CHANNEL          8   ///< PID Ki channel
+#define RC_CHANNELS         7   ///< Number of channels, modify according to RC
 #else
 #error Aircraft model undefined !
 #endif
@@ -73,6 +75,7 @@
 #define MODE_NAV            2   ///< navigate
 #define MODE_MANUAL         3   ///< manual control
 #define MODE_RTL            4   ///< return to launch
+#define MODE_FPV            5   ///< stabilize camera for FPV
 
 /*----------------------------------- Macros ---------------------------------*/
 
